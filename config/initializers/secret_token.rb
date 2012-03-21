cdbe59ade7c0f10ea79553c3a90dbba93b3e22f5
Robert Poor <rdpoor@gmail.com>
2012-03-21 13:56:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprunger::Application.config.secret_token = '0aca64ccecf169e4d1a0e349504174d57fc31a89a0a36873f0fcf58e0ebdc8dc57fb88d2eb38bdf3da70d00ac585b05844a0899025a483cb06ace6bcb65d1745'
